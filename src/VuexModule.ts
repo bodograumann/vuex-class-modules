83017b5b43bc30c2c01548d713ce58189cf49a55
Bodo Graumann <mail@bodograumann.de>
2019-01-11 13:09:52 +0100

    Allow options passed as factory function

--- src/VuexModule.ts
+++ src/VuexModule.ts
@@ -2,7 +2,10 @@ import { RegisterOptions } from "./module-factory";
 
 export class VuexModule {
   private __options: RegisterOptions;
-  constructor(options: RegisterOptions) {
+  constructor(options: RegisterOptions | ((className: string) => RegisterOptions)) {
+    if (typeof options === "function") {
+      options = options(this.constructor.name);
+    }
     this.__options = options;
   }
 }
